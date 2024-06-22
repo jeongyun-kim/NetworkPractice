9abcf3e460c6913eafeb7cd0b02bb84e55fecdd9
jeongyun-kim <kkanyo04@naver.com>
2024-06-22 14:03:53 +0900

    Light 모드 고정 / 네트워크 코드 수정

--- NetworkPractice/Resources/NetworkService.swift
+++ NetworkPractice/Resources/NetworkService.swift
@@ -25,28 +25,20 @@ class NetworkService {
 
 extension NetworkService {
     func fetchLottoData(handler: @escaping (Lotto) -> Void) {
-        fetch(NetworkCase: .lotto, params: nil, headers: nil) { result in
-            handler(result)
-        }
+        fetch(NetworkCase: .lotto, params: nil, headers: nil, completionHandler: handler)
     }
     
     func fetchWeatherData(x: Double, y: Double, handler: @escaping (WeatherContainer) -> Void) {
         let params: Parameters = ["lon": x, "lat": y, "appid": APIKeys.weatherKey, "lang": "kr"]
-        fetch(NetworkCase: .openWeather, params: params, headers: nil) { result in
-            handler(result)
-        }
+        fetch(NetworkCase: .openWeather, params: params, headers: nil, completionHandler: handler)
     }
     
     func fetchAddressData(x: Double, y: Double, handler: @escaping (AddressContainer) -> Void) {
         let params: Parameters = ["x": x, "y": y]
-        fetch(NetworkCase: .kakaoAddress, params: params, headers: KakaoUrl.kakaoHeaders) { result in
-            handler(result)
-        }
+        fetch(NetworkCase: .kakaoAddress, params: params, headers: KakaoUrl.kakaoHeaders, completionHandler: handler)
     }
     
     func fetchMovieData(handler: @escaping (BoxOffice) -> Void) {
-        fetch(NetworkCase: .movie, params: nil, headers: nil) { result in
-            handler(result)
-        }
+        fetch(NetworkCase: .movie, params: nil, headers: nil, completionHandler: handler)
     }
 }
